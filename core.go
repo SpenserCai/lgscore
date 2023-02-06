4c8782ee79956b4db518a583e825eb314a2a107b
SpenserCai <spensercai@gmail.com>
2023-02-06 09:47:40 +0800

    init lgscore

---
+++ core.go
@@ -0,0 +1,9 @@
+/*
+ * @Author: SpenserCai
+ * @Date: 2023-02-06 09:47:07
+ * @version:
+ * @LastEditors: SpenserCai
+ * @LastEditTime: 2023-02-06 09:47:26
+ * @Description: file content
+ */
+package lgscore
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/SpenserCai/lgscore
+
+go 1.19
